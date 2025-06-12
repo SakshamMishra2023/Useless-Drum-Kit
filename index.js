5392b65b1b5cd738c2748668f59ec68ee621d9c5
Saksham Mishra <140255588+SakshamMishra2023@users.noreply.github.com>
2025-06-13 00:14:59 +0530

    first commit of th

---
+++ index.js
@@ -0,0 +1,66 @@
+
+
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+
+        var buttonInnerHTML = this.innerHTML;
+
+        makeSound(buttonInnerHTML);
+
+
+    
+    });
+}
+
+document.addEventListener("keydown", function(event) {
+    makeSound(event.key);
+
+});
+
+function makeSound(key) {
+    switch(key){
+        case "w":
+            var audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
+            break;
+        case "a":
+            var audio = new Audio("sounds/tom-2.mp3");
+            audio.play();
+            break;
+        case "s":
+            var audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
+            break;
+        case "d":
+            var audio = new Audio("sounds/tom-4.mp3");
+            audio.play();
+            break;
+        case "j":
+            var audio = new Audio("sounds/snare.mp3");
+            audio.play();
+            break;
+        case "k":    
+            var audio = new Audio("sounds/crash.mp3");
+            audio.play();
+            break;
+        case "l":
+            var audio = new Audio("sounds/kick-bass.mp3");
+            audio.play();
+            break;
+    }
+}
+
+
+
+//operator use in js
+/* 
+    function add(num1, num2) {
+        return num1 + num2;
+    }
+    function multiply(num1, num2) {
+        return num1 * num2;
+    }
+    function calculate(num1, num2, operator) {
+        return operator(num1, num2);
+    } 
+*/
